45701c7fe3e3f3466cc6f1537495439a5069cf4f
antontx <tarisjan@gmail.com>
2023-03-13 19:12:49 +0100

    Add project files.

---
+++ killSubfolders/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+class Program {
+    
+    static void Main(string[] args)
+    {
+        string sourceDirectory = @"D:\files\documents\folderA";
+        string destinationDirectory = @"D:\files\documents\folderB";
+
+        int count = collectFiles(sourceDirectory, destinationDirectory);
+        Console.WriteLine("done. " + count + " files copied to " + destinationDirectory);
+    }
+
+    private static int collectFiles(string sourceDirectory, string destinationDirectory)
+    {
+        int filesCopied = 0;
+
+        try {
+
+            string[] content = Directory.GetFileSystemEntries(sourceDirectory);
+
+            foreach (string item in content) {
+                if (File.Exists(item))
+                {
+
+                    string fileName = Path.GetFileName(item);
+                    File.Copy(item,Path.Combine(destinationDirectory, fileName));
+                    filesCopied++;
+
+
+
+                } else if (Directory.Exists(item))
+                {
+                    filesCopied += collectFiles(item, destinationDirectory);
+                }
+
+            }
+
+        }catch (Exception e) { Console.WriteLine(e.Message); }
+
+        return filesCopied;
+    }
+}
